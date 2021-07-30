48097f6ff6df694908d8ee2b364332aff1e97c1a
Mamatha <mamatha.vurise@gmail.com>
2021-07-31 01:24:50 +0530

    first commit

---
+++ src/app/employee.ts
@@ -0,0 +1,10 @@
+export class Employee {
+    id:number;
+    firstName:string;;
+    lastName:string;
+    emailId:string;
+    address:string;
+    state:string;
+    pincode:number;
+    mobileNo:number;
+}
